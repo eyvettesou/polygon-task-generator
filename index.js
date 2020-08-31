70bd592bf0ea1fe1006f6b15872aea611bb01c7b
Eyvette Sou <eyvette.sou@procore.com>
2020-08-30 19:54:42 -0700

    Generate base of app
    Adds SVG assets, adds basic logic for generating randomized polygons, adds basic layout

---
+++ index.js
@@ -0,0 +1,135 @@
+const shapesMap = {
+  6: {
+    sides: 6,
+    svg: 'assets/hexagon-regular.svg',
+  },
+  7: {
+    sides: 7,
+    svg: 'assets/heptagon-regular.svg',
+  },
+  8: {
+    sides: 8,
+    svg: 'assets/octagon-regular.svg',
+  },
+  9: {
+    sides: 9,
+    svg: 'assets/nonagon-regular.svg',
+  },
+  10: {
+    sides: 10,
+    svg: 'assets/decagon-regular.svg',
+  },
+}
+
+// TO DO: Add drop down that will take in range values for sides
+// TO DO: Add input of total number of rows
+// TO DO: Add input for total number of items per ro
+// TO DO: Add error check if not evenly divisible
+// TO DO: Add button for generating shapes
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomShapes({min: min, max: max, totalNum: totalNum}) {
+  let index;
+  const shapes = [];
+  const shapesCount = {};
+
+  for (index = 0; index < totalNum; index++) {
+    const randomShape = getRandomInt(min, max);
+    shapesCount[randomShape] = shapesCount[randomShape] + 1 || 1;
+    shapes.push(randomShape);
+  }
+
+  return {
+    shapes,
+    shapesCount,
+  }
+}
+
+function createContentContainer() {
+  const content = document.createElement('div');
+  content.classList.add('content-container');
+  return content;
+}
+
+function createShapeContainer() {
+  const shapeContent = document.createElement('div');
+  shapeContent.classList.add('shape-container');
+  return shapeContent;
+}
+
+function createRow() {
+  const newRow = document.createElement('div');
+  newRow.classList.add('row-container');
+  return newRow;
+}
+
+function createShape(shape) {
+  const itemContainer = document.createElement('div');
+  itemContainer.classList.add('item-container');
+  const img = document.createElement('img');
+  img.src = shapesMap[shape].svg;
+  img.width = 75;
+  const randomDegreesValue = getRandomInt(5, 175);
+  img.style.transform = `rotate(${randomDegreesValue}deg)`;
+  itemContainer.appendChild(img);
+  return itemContainer
+}
+
+function createShapesContent(shapes, shapeContainer){
+  let row;
+
+  shapes.forEach((shape, index) => {
+    if (index === 0 || !(index % 8)) {
+      row = createRow();
+      shapeContainer.appendChild(row);
+    }
+    const newShape = createShape(shape);
+    row.appendChild(newShape);
+  })
+}
+
+function createSolutionBlock(shapesCount) {
+  const solution = document.createElement('div');
+  solution.classList.add('solution')
+  const solutionHeader = document.createElement('h2');
+  solutionHeader.innerHTML = 'Solution:';
+  const solutionBody = document.createElement('p');
+  solutionBody.classList.add('solutionBody')
+  solutionBody.innerHTML = `
+  6 sides: ${shapesCount[6] || 0}, <br/>
+  7 sides: ${shapesCount[7]}, <br/>
+  8 sides: ${shapesCount[8]}, <br/>
+  9 sides: ${shapesCount[9]}, <br/>
+  10 sides: ${shapesCount[10]}, <br/>
+  `;
+  solution.appendChild(solutionHeader)
+  solution.appendChild(solutionBody)
+  content.appendChild(solution)
+}
+
+const content = createContentContainer();
+const shapeContainer = createShapeContainer();
+content.appendChild(shapeContainer);
+
+const minSide = 7;
+const maxSide = 10
+const totalNumOfShapes = 24;
+
+const { shapes: shapesList, shapesCount } = getRandomShapes({
+  min: minSide,
+  max: maxSide,
+  totalNum: 24
+});
+
+createShapesContent(shapesList, shapeContainer);
+createSolutionBlock(shapesCount);
+
+const app = document.getElementById('content');
+app.appendChild(content)
+

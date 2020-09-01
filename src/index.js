f8c733f9254414f32637e38d542e8c8ae99ccb13
Eyvette Sou <eyvette.sou@procore.com>
2020-09-01 14:13:40 -0700

    Lots of refatoring to get libraries

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
+import html2canvas from "html2canvas";
+
 const shapesMap = {
   6: {
     sides: 6,
-    svg: 'assets/hexagon-regular.svg',
+    svg: "assets/hexagon-regular.svg",
   },
   7: {
     sides: 7,
-    svg: 'assets/heptagon-regular.svg',
+    svg: "assets/heptagon-regular.svg",
   },
   8: {
     sides: 8,
-    svg: 'assets/octagon-regular.svg',
+    svg: "assets/octagon-regular.svg",
   },
   9: {
     sides: 9,
-    svg: 'assets/nonagon-regular.svg',
+    svg: "assets/nonagon-regular.svg",
   },
   10: {
     sides: 10,
-    svg: 'assets/decagon-regular.svg',
+    svg: "assets/decagon-regular.svg",
   },
-}
+};
 
 // TO DO: Add drop down that will take in range values for sides
 // TO DO: Add input of total number of rows
@@ -48,59 +50,65 @@ function getRandomShapes({min: min, max: max, totalNum: totalNum}) {
   return {
     shapes,
     shapesCount,
-  }
+  };
 }
 
 function createContentContainer() {
-  const content = document.createElement('div');
-  content.classList.add('content-container');
+  const content = document.createElement("div");
+  content.classList.add("content-container");
   return content;
 }
 
 function createShapeContainer() {
-  const shapeContent = document.createElement('div');
-  shapeContent.classList.add('shape-container');
+  const shapeContent = document.createElement("div");
+  shapeContent.id = "shape-content";
   return shapeContent;
 }
 
 function createRow() {
-  const newRow = document.createElement('div');
-  newRow.classList.add('row-container');
+  const newRow = document.createElement("div");
+  newRow.classList.add("row-container");
   return newRow;
 }
 
 function createShape(shape) {
-  const itemContainer = document.createElement('div');
-  itemContainer.classList.add('item-container');
-  const img = document.createElement('img');
+  const itemContainer = document.createElement("div");
+  itemContainer.classList.add("item-container");
+  const img = document.createElement("img");
   img.src = shapesMap[shape].svg;
   img.width = 75;
   const randomDegreesValue = getRandomInt(5, 175);
   img.style.transform = `rotate(${randomDegreesValue}deg)`;
   itemContainer.appendChild(img);
-  return itemContainer
+  return itemContainer;
 }
 
 function createShapesContent(shapes, shapeContainer){
+  const shapeSVGs = document.createElement("div");
+  shapeSVGs.classList.add("shape-container");
+  shapeSVGs.id = "shape-svgs";
+  shapeContainer.appendChild(shapeSVGs);
+
   let row;
 
   shapes.forEach((shape, index) => {
     if (index === 0 || !(index % 8)) {
       row = createRow();
-      shapeContainer.appendChild(row);
+      shapeSVGs.appendChild(row);
     }
     const newShape = createShape(shape);
     row.appendChild(newShape);
-  })
+  });
+  
 }
 
 function createSolutionBlock(shapesCount) {
-  const solution = document.createElement('div');
-  solution.classList.add('solution')
-  const solutionHeader = document.createElement('h2');
-  solutionHeader.innerHTML = 'Solution:';
-  const solutionBody = document.createElement('p');
-  solutionBody.classList.add('solutionBody')
+  const solution = document.createElement("div");
+  solution.classList.add("solution");
+  const solutionHeader = document.createElement("h2");
+  solutionHeader.innerHTML = "Solution:";
+  const solutionBody = document.createElement("p");
+  solutionBody.classList.add("solutionBody");
   solutionBody.innerHTML = `
   6 sides: ${shapesCount[6] || 0}, <br/>
   7 sides: ${shapesCount[7]}, <br/>
@@ -108,9 +116,9 @@ function createSolutionBlock(shapesCount) {
   9 sides: ${shapesCount[9]}, <br/>
   10 sides: ${shapesCount[10]}, <br/>
   `;
-  solution.appendChild(solutionHeader)
-  solution.appendChild(solutionBody)
-  content.appendChild(solution)
+  solution.appendChild(solutionHeader);
+  solution.appendChild(solutionBody);
+  content.appendChild(solution);
 }
 
 const content = createContentContainer();
@@ -118,18 +126,26 @@ const shapeContainer = createShapeContainer();
 content.appendChild(shapeContainer);
 
 const minSide = 7;
-const maxSide = 10
+const maxSide = 10;
 const totalNumOfShapes = 24;
 
 const { shapes: shapesList, shapesCount } = getRandomShapes({
   min: minSide,
   max: maxSide,
-  totalNum: 24
+  totalNum: totalNumOfShapes
 });
 
 createShapesContent(shapesList, shapeContainer);
 createSolutionBlock(shapesCount);
 
-const app = document.getElementById('content');
-app.appendChild(content)
+const app = document.getElementById("content");
+app.appendChild(content);
 
+document.onload = function() {
+  const shapeContent = document.getElementById("shape-content");
+  const shapeSVGs = document.getElementById("shape-svgs");
+  html2canvas(shapeContent).then(function(canvas) {
+      shapeContent.removeChild(shapeSVGs);
+      shapeContent.appendChild(canvas);
+  });
+};
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(svg)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: "assets/",
+                            name: "[path][name].[ext]",
+                        }
+                    }
+                ],
+            },
+            {    
+                test: /\.(svg)$/,
+                exclude: /fonts/,
+                use: [
+                    {
+                        loader: "svg-url-loader",
+                        options: {
+                            noquotes: true,
+                            outputPath: "assets/",
+                            name: "[path][name].[ext]",
+                        },
+                    },
+                ],
+            },
+            {
+                enforce: "pre",
+                test: /.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader"
+            },
+            {
+                test: /.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    }
+};
